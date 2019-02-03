4077ee2006b814268e680b32aaf5c33a888fe6e2
Steve Lee <slee@microsoft.com>
2019-02-02 18:47:54 -0800

    have poshchan check who sent the message to avoid recursive loop based on her posts

--- FunctionApp/AzDevOps/run.ps1
+++ FunctionApp/AzDevOps/run.ps1
@@ -1,16 +1,54 @@
 using namespace System.Net
+using namespace System.Web
 
 # Input bindings are passed in via param block.
 param($Request, $TriggerMetadata)
 
 # Write to the Azure Functions log stream.
-Write-Host "PowerShell HTTP trigger function processed a request."
+Write-Host "PoshChan-Bot received a request"
 
-$status = [HttpStatusCode]::OK
-$body = $Request
+# Change to get list of authorized users from specific repo
+function Test-User([string] $user) {
+    return $user -in @(
+        "SteveL-MSFT"
+    )
+}
+
+$body = $Request.Body
+$poshchanMention = "@PoshChan "
+
+# Change to get list of action verbs mapped to webhooks
+if ($body.action -eq "created") {
+    $user = $body.comment.user.login
+    if (Test-User $user) {
+        $commentBody = $body.comment.body
+        if ($commentBody.StartsWith($poshchanMention)) {
+            $command = $commentBody.SubString($poshchanMention.Length)
+            $output = @(
+                "Authorized User: $user"
+                "Command: $command"
+                "PR: $($body.issue.pull_request.url)"
+            )
+            $output = [string]::Join("`n", $output)
+            $output | Write-Host
+            $output = [HttpUtility]::JavaScriptStringEncode($output)
+
+            try {
+                Invoke-RestMethod -Headers @{Authorization = "token $($env:GITHUB_API_KEY)"} $body.issue.comments_url -Method Post -Body "{ ""body"": ""$output"" }"
+            } catch {
+                $_ | Out-String | Write-Host
+            }
+        } else {
+            Write-Host "Ignoring comment not directed @PoshChan"
+        }
+    } else {
+        Write-Warning "Unauthorized User: $user"
+    }
+} else {
+    Write-Host "Ignoring action $($body.action)"
+}
 
 # Associate values to output bindings by calling 'Push-OutputBinding'.
 Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
-    StatusCode = $status
-    Body = $body
+    StatusCode = [HttpStatusCode]::OK
 })
