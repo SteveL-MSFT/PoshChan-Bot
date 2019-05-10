8539667658c239bd8256180675ea129f16d24ee4
Steve Lee <slee@microsoft.com>
2019-05-10 11:25:48 -0700

    add reactions instead of message for ack

--- FunctionApp/GitHub-Respond/run.ps1
+++ FunctionApp/GitHub-Respond/run.ps1
@@ -3,21 +3,32 @@ param($QueueItem, $TriggerMetadata)
 
 $url = $QueueItem.url
 $message = $QueueItem.message
+$reaction = $QueueItem.reaction
 
-if ($null -eq $message) {
-    Write-Error "Message is missing"
-    return
+if ($message) {
+    Write-Host "Posting message:`n$message"
+    Write-Host "To URL: $url"
+
+    $json = @{
+        body = $message
+    } | ConvertTo-Json -Compress
+
+    try {
+        Send-GitHubComment -Url $url -Body $json
+    } catch {
+        $_ | Out-String | Write-Error
+    }
 }
+elseif ($reaction) {
+    Write-Host "Posting reaction: $reaction"
+    Write-Host "To URL: $url"
 
-Write-Host "Posting message:`n$message"
-Write-Host "To URL: $url"
-
-$json = @{
-    body = $message
-} | ConvertTo-Json -Compress
-
-try {
-    Send-GitHubComment -Url $url -Body $json
-} catch {
-    $_ | Out-String | Write-Error
+    try {
+        Send-GitHubReaction -Url $url -Reaction $reaction
+    } catch {
+        $_ | Out-String | Write-Error
+    }
+}
+else {
+    Write-Error "Message and Reaction is missing"
 }
